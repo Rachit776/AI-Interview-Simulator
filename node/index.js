f582acfaf7d7e66e74563f2a3167f1212adb1a4e
Rachit776 <rachitkumar776@gmail.com>
2025-05-18 12:04:22 +0530

    node/index.js modified

--- node/index.js
+++ node/index.js
@@ -9,22 +9,24 @@ import authRouter from "./routes/authRouter.js";
 import audioRouter from "./routes/audioRouter.js";
 import { route as processedMessageRoute } from "./routes/processedMessageRoute.js";
 import { startConsumer } from "./utils/consumer.js";
-import "./utils/passport.js"; // Make sure this line is importing your passport.js file
+import "./utils/passport.js";
 
 const app = express();
 dotenv.config();
+
+const CLIENT_URL = process.env.CLIENT_URL;
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
 	res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
 	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+	res.setHeader("Access-Control-Allow-Origin", CLIENT_URL);
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
 	next();
 });
+
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,
@@ -35,23 +37,21 @@ app.use(
 
 app.use(
 	cors({
-		origin: "http://localhost:5173", // Replace with your frontend's URL
-		methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-		credentials: true, // Allow credentials (if needed)
+		origin: CLIENT_URL,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
 	})
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use("/auth", authRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/audio", audioRouter);
-app.use("/api/messages", processedMessageRoute); // New route
+app.use("/api/messages", processedMessageRoute);
 
-// Endpoint to process messages received from RabbitMQ
 app.post("/exchange", (req, res) => {
 	const { message } = req.body;
-	// console.log(`[*] Message received: ${message}`);
 	res.json({ status: "Message processed", message });
 });
 
