bad2418008a2f526377e026c3fa4bf2eabe1038b
Rachit776 <rachitkumar776@gmail.com>
2025-05-18 12:34:48 +0530

    Typo Error

--- node/utils/consumer.js
+++ node/utils/consumer.js
@@ -20,7 +20,7 @@ export async function startConsumer() {
   try {
     //const connection = await amqp.connect(rabbitmq_url);
     const connection = await amqp.connect({
-      protocol: 'amps',
+      protocol: 'amqps',
       hostname: process.env.RABBITMQ_HOST,
       port: parseInt(process.env.RABBITMQ_PORT),
       username: process.env.RABBITMQ_USER,
